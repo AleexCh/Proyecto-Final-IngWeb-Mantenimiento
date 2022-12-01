87b58539a1b704a1957c2513cdf361698db90de7
AleexCh <achung95@hotmail.com>
2022-12-01 09:06:08 -0500

    Error, el modelo si necesita de los atributos para la pagina

--- model/Teams.php
+++ model/Teams.php
@@ -10,11 +10,21 @@ class Teams extends BaseModel
     public int | null $id;
     public string | null $country;
     public string | null $group;
+    public string | null $win;
+    public string | null $draw;
+    public string | null $loss;
+    public string | null $goals_favor;
+    public string | null $goals_againts;
 
     public function __construct($args = [])
     {
         $this->id = ($args["id"] ?? null);
         $this->country = ($args["country"] ?? null);
         $this->group = ($args["group"] ?? null);
+        $this->win = ($args["win"] ?? null);
+        $this->draw = ($args["draw"] ?? null);
+        $this->loss = ($args["loss"] ?? null);
+        $this->goals_favor = ($args["goals_favor"] ?? null);
+        $this->goals_againts = ($args["goals_againts"] ?? null);
     }
 }
